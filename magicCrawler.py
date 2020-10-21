e94b9bced181b7ed750ff5758b607862656da9b5
David Schmitt <david.schmitt@rwth-aachen.de>
2020-10-21 17:22:26 -0400

    Can find a list of all legendary creatures

--- magicCrawler.py
+++ magicCrawler.py
@@ -2,28 +2,43 @@ import urllib.request
 import json
 import requests
 
-baseurl = 'https://api.scryfall.com/cards/named?exact='
-# cardname = 'Marchesa-the-black-rose'
-cardname = 'marchesa the black rose'
+baseurl = 'https://api.scryfall.com/cards/'
 
-url = (baseurl + cardname).encode()
+# Search for cardname:
+# options = 'named?exact='
+# cardname = 'marchesa the black rose'
+# url = (baseurl + options + cardname).encode()
 
+options = 'search?order=set&q=t:creature t:legend&dir=desc'
+url = (baseurl + options).encode()
+
+# I am just looking through the first page
+# add logic for second page
 print('request url: ', url)
 response = requests.get(url)
 # print(type(response))
 # print(response.text)
+if response.status_code == 200:
+    jcards = response.json()
+    printedcards = 0
+    # print(jcards)
+    print('------------------')
+    for bla in jcards['data']:
+        print(bla.get('name'))
+        print('------------------')
+        printedcards += 1
 
-jcards = response.json()
+    print('I found: ' , jcards['total_cards'],' total cards')
+    print('cards printed: ', printedcards)
+else:
+    print(response)
 
-# print(jcards)
-print('------------------')
-# for bla in jcards:
-    # print(bla)
 
-print('name', jcards['name'])
-print('cmc', jcards['cmc'])
-print('mana_cost', jcards['mana_cost'])
-print('image_uris', jcards['image_uris']['normal'])
+
+# print('name', jcards['name'])
+# print('cmc', jcards['cmc'])
+# print('mana_cost', jcards['mana_cost'])
+# print('image_uris', jcards['image_uris']['normal'])
 
 
 # user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
